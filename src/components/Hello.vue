2dfa9dc771c1effc35b60c2dbb717e54056429ec
Роман <arrumanov@outlook.com>
2018-02-10 13:54:33 +0300

    commit fifth

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,7 +5,42 @@
       <kendo-buttongroup-button> Button 2 </kendo-buttongroup-button>
       <kendo-buttongroup-button> Button 3 </kendo-buttongroup-button>
     </kendo-buttongroup>
-  </div>
+    <div class="grid">
+      <kendo-datasource 
+        ref="datasource"
+        :type="'odata'"
+        :pageSize="20"
+        :transportRead="'https://demos.telerik.com/kendo-ui/service/Northwind.svc/Customers'"
+      ></kendo-datasource>
+      <kendo-grid
+        :height="550"
+        :dataSourceRef="'datasource'"
+        :groupable='true'
+        :sortable='true'
+        :pagearble-refresh='true'
+        :pageable-page-sizes='true'
+        :pageable-button-count='5'>
+      <kendo-grid-column
+        field="ContactName"
+        title="Contact name"
+        :width="250">
+      </kendo-grid-column>
+      <kendo-grid-column
+        field="ContactTitle"
+        title="Contact title">
+      </kendo-grid-column>
+      <kendo-grid-column
+        field="CompanyName"
+        title="Company name">
+      </kendo-grid-column>
+      <kendo-grid-column
+        field="County"
+        title="County"
+        :width="150">
+      </kendo-grid-column>
+      </kendo-grid>
+    </div>
+  </div> 
 </template>
 
 <script>
